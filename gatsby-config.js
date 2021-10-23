9c023685e1ddc6ac852388bd786455433eb27f1a
bru170 <80203072+bru170@users.noreply.github.com>
2021-10-23 17:31:45 +0200

    build project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,39 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "sos-digital-welfare",
+    title: "sos-digital-welfare"
   },
-  plugins: ["gatsby-plugin-styled-components"],
-};
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: `http://localhost:10018/graphql`,
+        hostingWPCOM: false
+      }
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`
+      },
+      __key: "images"
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Fira Sans\:200, 400, 500, 600, 700, 800, 900`,
+          `Helvetica Neue\:200, 400, 500, 600, 700, 800, 900`,
+          `sans-serif\:200, 400, 500, 600, 700, 800, 900`
+        ],
+        display: `swap`
+      }
+    }
+  ]
+}
